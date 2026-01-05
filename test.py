329c7b815f3ade1aadb69bba5620c999fd40def7
shifeng1992 <13241657+shifeng1992@user.noreply.gitee.com>
2026-01-05 23:25:18 +0800

    测试了一下普通api

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 from google import genai
 
 client = genai.Client()
+chat = client.chats.create(model="gemini-3-flash-preview")
 
-interaction =  client.interactions.create(
-    model="gemini-3-pro-preview",
-    input="Tell me a short joke about programming."
-)
+response = chat.send_message("我的硕士专业是西南交通大学的土木工程")
+print(response.text)
 
-print(interaction.outputs[-1].text)
+response = chat.send_message("可是我2018年转行了，因为我感觉土木不行了，事实情况也是如此，你赞同吗？")
+print(response.text)
+
+for message in chat.get_history():
+    print(f'role - {message.role}',end=": ")
+    print(message.parts[0].text)
